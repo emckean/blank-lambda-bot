f43835843e88c0c229fc5487ccf2d06f4653c557
Erin McKean <erin@logocracy.com>
2016-06-11 17:46:33 -0700

    adding temp config files

---
+++ botfiles/temp-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //Your Twitter credentials go in this file
+  // consumer_key:         '',
+  // consumer_secret:      '',
+  // access_token:         '',
+  // access_token_secret:  ''
+};
